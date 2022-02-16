99fc282df400dd8b0eb09298083ae46d87a3e635
Laurent LEFEVRE <koubyak@msn.com>
2022-02-16 19:04:34 +0100

    fix some issues

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,3 +24,17 @@ function backToTop() {
         behavior: "smooth",
       });
 }
+
+// Manage Tags Tabs
+if(window.location.hash) {
+  // Get HashTag from URL
+  var hash = window.location.hash;
+  // Search Tab 
+  hash += '-tab';
+  var hashTabTrigger = document.querySelector(hash);
+  // If Tab exist, open content
+  if(hashTabTrigger){
+      var hashTab = bootstrap.Tab.getOrCreateInstance(hashTabTrigger)
+      hashTab.show()
+  }
+}
